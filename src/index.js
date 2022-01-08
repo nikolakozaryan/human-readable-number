a24d9600b10aef84d7aea9b26cb61dda9ad3dcec
Nikolay Kozyrev <kolyak770@gmail.com>
2022-01-08 23:01:03 +0300

    refactor: refactor solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
-  let ones = number % 10,
-        decs = ((number - ones) % 100) / 10,
-        hundreds = ((number - (number - ones) % 100 - ones) % 1000) / 100,
-        resDecs, resOnes;
+  let hundreds = Math.trunc(number/100),
+      decs     = Math.trunc(number / 10 - hundreds * 10),
+      ones     = number % 10,
+      resHundreds, resDecs, resOnes;
 
   const digits = {
       0: 'zero',
@@ -18,42 +18,45 @@ module.exports = function toReadable (number) {
   }
 
   resHundreds = hundreds ? `${digits[hundreds]} hundred` : '';
-  
-  if (decs == 1 || (!decs && !ones) || (decs && !ones)) resOnes = '';
-  else resOnes = digits[ones];
-  
+
   switch (decs) {
-      case 0:
-          resDecs = '';
-          break;
-      case 1:
-          if (ones == 0) resDecs = 'ten';
-          else if (ones == 1) resDecs = 'eleven';
-          else if (ones == 2) resDecs = 'twelve';
-          else if (ones == 3) resDecs = 'thirteen';
-          else if (ones == 5) resDecs = 'fifteen';
-          else if (ones == 8) resDecs = 'eighteen';
-          else resDecs = `${digits[ones]}teen`;
-          break;
-      case 2:
-          resDecs = 'twenty';
-          break;
-      case 3:
-          resDecs = 'thirty';
-          break;
-      case 4: 
-          resDecs = 'forty';
-          break;
-      case 5:
-          resDecs = 'fifty';
-          break;
-      case 8: 
-          resDecs = 'eighty';
-          break;
-      default: resDecs = `${digits[decs]}ty`
+    case 0:
+        resDecs = '';
+        break;
+    case 1:
+        if (ones == 0) resDecs = 'ten';
+        else if (ones == 1) resDecs = 'eleven';
+        else if (ones == 2) resDecs = 'twelve';
+        else if (ones == 3) resDecs = 'thirteen';
+        else if (ones == 5) resDecs = 'fifteen';
+        else if (ones == 8) resDecs = 'eighteen';
+        else resDecs = `${digits[ones]}teen`;
+        break;
+    case 2:
+        resDecs = 'twenty';
+        break;
+    case 3:
+        resDecs = 'thirty';
+        break;
+    case 4: 
+        resDecs = 'forty';
+        break;
+    case 5:
+        resDecs = 'fifty';
+        break;
+    case 8: 
+        resDecs = 'eighty';
+        break;
+    default: resDecs = `${digits[decs]}ty`
   }
 
-  return number === 0 ? 'zero' : `${resHundreds}${resHundreds && (resDecs || resOnes) ? ' ' : ''}${resDecs}${!resOnes || (resHundreds && !resDecs) || (!resHundreds && !resDecs) ? '' : ' '}${resOnes}`   
+  resOnes = (decs == 1 || !ones) ? '' : digits[ones];
+
+  return number === 0 ? 'zero' : resHundreds + 
+                                 `${resHundreds && (resDecs || resOnes) ? ' ' : ''}` + 
+                                 resDecs + 
+                                 `${resOnes && resDecs ? ' ' : ''}` + 
+                                 resOnes
 }
 
 //I FILL MYSELF STUPID, BUT IT WORKS AND I'M HAPPY
